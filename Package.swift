104bf9a703910cb6bf32e6900969b731e8540008
skipp <chanchanakoedtho@gmail.com>
2022-08-04 19:31:13 +0700

    Fixed package

--- Package.swift
+++ Package.swift
@@ -11,13 +11,10 @@ let package = Package(
             name: "GoogleMapsUtils",
             targets: ["GoogleMapsUtils"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/YAtechnologies/GoogleMaps-SP.git", .upToNextMinor(from: "6.0.0"))
-    ],
     targets: [
         .binaryTarget(
             name: "GoogleMapsUtils",
-            url: "https://github.com/sk-chanch/GoogleMapsUtils-SPM/files/4.0.1/GoogleMapsUtils.xcframework.zip",
+            url: "https://github.com/sk-chanch/GoogleMapsUtils-SPM/releases/download/4.1.0/GoogleMapsUtils.xcframework.zip",
             checksum: "fb55b6bdf300d562048729b47951f04398a9c5ebc21ee18f89945d0d7bd66279"
         )
         
